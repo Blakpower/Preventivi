8e551dc60a4ce944b80a466b50e5ae68530d5cbd
Fabio Porto <blakpower@MacBook-Air-M2-di-Fabio.local>
2026-01-26 17:41:35 +0100

    update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // These should be in environment variables in a real app
 const supabaseUrl = 'https://szzcfuytachorchfbsvp.supabase.co';
-const supabaseKey = 'sb_publishable_rm4U8v4NJ8ArnShVv1oj7Q_HGjXvZS2';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6emNmdXl0YWNob3JjaGZic3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNjUyMTAsImV4cCI6MjA4NDY0MTIxMH0.TwjvmIg43syQgClFAo0XjeUf195Minw761LF00xtgzY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
